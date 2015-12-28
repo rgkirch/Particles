4ae01427c9437096fdfa9315cec9b3e9d3af5844
Richie Kirchofer <rgkirch@gmail.com>
2015-12-28 17:16:12 -0500

    forgot charger, switching computers

--- new/soa.c
+++ new/soa.c
@@ -3,36 +3,51 @@
 #include <time.h>
 //#include <vector>
 
-#define numberOfParticles 1000
+#define DEFAULTNUMBEROFPARTICLES 500
 #define PI 3.1415926
 
 typedef struct {
-	float x[numberOfParticles];
-	float y[numberOfParticles];
-	float z[numberOfParticles];
+	float* x;
+	float* y;
+	float* z;
 } Velocity;
 
 typedef struct {
-	float x[numberOfParticles];
-	float y[numberOfParticles];
-	float z[numberOfParticles];
+	float* x;
+	float* y;
+	float* z;
 } Position;
 
-void initialize( float* begin, float* end ) {
+void randomize( float* beginingOfArray, float* endOfArray );
+Velocity* velocity_constructor( void );
+Position* position_constructor( void );
+
+int main(int argc, char** argv) {
+    //srand(time(NULL));
+    //FILE* file = fopen("test", "w");
+    //fprintf(file, "%f", rand()/(float)RAND_MAX);
+    int numberOfParticles;
+    //scanf("%d", &num);
+    if( argc > 1 ) {
+        numberOfParticles = atoi(argv[1]);
+    } else {
+        numberOfParticles = DEFAULTNUMBEROFPARTICLES;
+    }
+
+	Velocity velocity;
+	Position position;
+    for(int i = 0; i < 3; ++i) {
+        randomize( (float*)(&velocity), (float*)(&velocity)+sizeof(velocity)/sizeof(float) );
+    }
+
+	return 0;
+}
+
+void randomize( float* begin, float* end ) {
 	for( float* iter = begin; iter != end; ++iter ) {
 		*iter = rand()/(float)RAND_MAX;
 	}
 };
-
-int main() {
-    //srand(time(NULL));
-	Velocity velocity;
-	Position position;
-	initialize( (float*)(&velocity), (float*)(&velocity)+sizeof(velocity)/sizeof(float) );
-	initialize( (float*)(&position), (float*)(&position)+sizeof(position)/sizeof(float) );
-
-	return 0;
-}
 
 // thoughts - broadphase among x's, if x's are far enough apart, don't compare y's or z's
 // keep track of fastest particle, if the fastest particle couldn't cross half partition size since last repartition then safe to not repartition
