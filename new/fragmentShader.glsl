564a6928914e324ac9422a3f5132f6e4f211419a
Richie Kirchofer <rgkirch@gmail.com>
2015-12-28 18:05:50 -0500

    stuf

--- new/fragmentShader.glsl
+++ new/fragmentShader.glsl
@@ -1,13 +1,7 @@
 #version 330 core
 
-in vec2 twoTexCo;
 out vec4 color;
 
-uniform sampler2D texture;
-uniform sampler2D texture1;
-
 void main() {
-	//color = texture2D( texture1, twoTexCo );
-	color = mix( texture2D( texture, twoTexCo ), texture2D( texture1, twoTexCo ), 1.0f );
-	//color = vec4( 0.5f, 0.8f, 0.8f, 1.0f );
+	color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 }
---
+++ new/template.cpp
@@ -0,0 +1,184 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include <math.h>
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include "soa.h"
+
+using namespace std;
+
+void init(GLFWwindow* &theWindow, char* nameOfWindow, GLuint windowWidth, GLuint windowHeight);
+GLchar* readFile(char* fileName);
+GLuint createShader(char* vertexShaderFileName, char* fragmentShaderFileName);
+void printShaderLog(char* errorMessageWithoutNewline, GLuint shaderProgram);
+void checkShaderStepSuccess(GLint shaderProgramID, GLuint statusToCheck);
+
+int main(int argc, char** argv) {
+    const char* vertexShaderFileName = "vertexShader.glsl";
+    const char* fragmentShaderFileName = "fragmentShader.glsl";
+    const GLuint WIDTH = 1920*3/4, HEIGHT = 1080*3/4;
+    GLFWwindow* window;
+    init(window, argv[0], WIDTH, HEIGHT);
+
+    GLuint shaderProgram = createShader((char*)vertexShaderFileName, (char*)fragmentShaderFileName);
+    glUseProgram(shaderProgram);
+
+    glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
+
+    const GLfloat point[] = {0.0f, 0.0f, 0.0f};
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
+
+
+	while( !glfwWindowShouldClose( window ) ) {
+		glfwPollEvents();
+		glClear( GL_COLOR_BUFFER_BIT );
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glDrawArrays(GL_POINTS, 0, 3);
+
+        //glFlush();
+		glfwSwapBuffers( window );
+	}
+
+	glfwDestroyWindow( window );
+	glfwTerminate();
+	return 0;
+}
+
+// allocates memory for (shader) file contents
+GLchar* readFile(char* fileName) {
+    FILE* file = fopen(fileName, "rb");
+    if( ! file ) fprintf(stderr, "error: Could not open file %s\n", fileName);
+    long fileLength = 0;
+    fseek(file, 0, SEEK_END);
+    fileLength = ftell(file);
+    rewind(file);
+    GLchar* contents = (GLchar*)malloc(fileLength + 1);
+    if( ! contents ) fprintf(stderr, "error: Could not allocate memory.\n");
+    contents[fileLength] = '\0';
+    fread(contents, 1, fileLength, file);
+    fclose(file);
+    return contents;
+}
+
+GLuint createShader(char* vertexShaderFileName, char* fragmentShaderFileName) {
+    if( ! vertexShaderFileName ) fprintf(stderr, "error: NULL passed in for vertex shader file name.\n");
+    if( ! fragmentShaderFileName ) fprintf(stderr, "error: NULL passed in for fragment shader file name.\n");
+    char* sourceFileName[] = { vertexShaderFileName, fragmentShaderFileName };
+    GLint shaderEnums[] = { GL_VERTEX_SHADER, GL_FRAGMENT_SHADER };
+    GLuint program = glCreateProgram();
+    if( ! program ) fprintf(stderr, "error: failed to create program\n");
+    for(int i = 0; i < 2; ++i) {
+        GLchar* shaderCode = readFile( sourceFileName[i] );
+        GLuint shader = glCreateShader( shaderEnums[i] );
+        glShaderSource( shader, 1, (const GLchar**)&shaderCode, NULL );
+        glCompileShader(shader);
+        checkShaderStepSuccess(shader, GL_COMPILE_STATUS);
+        glAttachShader(program, shader);
+        free( shaderCode );
+        //delete [] shaderCode;
+        glDeleteShader(shader);
+    }
+    glLinkProgram(program);
+    checkShaderStepSuccess(program, GL_LINK_STATUS);
+    return program;
+}
+
+void checkShaderStepSuccess(GLint program, GLuint status) {
+    GLint success = -3;
+    switch(status) {
+        case GL_COMPILE_STATUS:
+            glGetShaderiv( program, status, &success );
+            if( success == -3 ) fprintf(stderr, "error: the success check may have a false positive\n");
+            if( ! success ) {
+                printShaderLog((char*)"error: gl shader program failed to compile.", program);
+                fprintf(stderr, "Exiting.\n");
+                exit(1);
+            }
+            break;
+        case GL_LINK_STATUS:
+            glGetProgramiv( program, status, &success );
+            printf("success value %d\n", success);
+            if( success == -3 ) fprintf(stderr, "error: the success check may have a false positive\n");
+            if( ! success ) {
+                printShaderLog((char*)"error: gl shader program failed to link.", program);
+                //fprintf(stderr, "Exiting.\n");
+                //exit(1);
+            }
+            break;
+        default:
+            fprintf(stderr, "function called with unhandled case.\n");
+            break;
+    }
+}
+
+void printShaderLog(char* errorMessage, GLuint shader) {
+    fprintf(stderr, "%s\n", errorMessage);
+    GLint length = 0;
+    glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &length );
+    printf("log length %d\n", length);
+    GLchar* logText = (GLchar*)malloc(sizeof(GLchar) * (length + 1));
+    logText[length] = '\0';
+    glGetShaderInfoLog(shader, length, &length, logText);
+    printf("printing log\n");
+    //fprintf(stderr, "%s", logText);
+    cout << logText << endl;
+    free(logText);
+}
+
+void init(GLFWwindow* &window, char* name, GLuint width, GLuint height) {
+	/* Initialize the library */
+	if ( !glfwInit() ) {
+        fprintf(stderr, "GLFW failed to init.\n");
+        fprintf(stderr, "Exiting.\n");
+        exit(1);
+	}
+
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow( width, height, name, NULL, NULL );
+	if ( !window ) {
+		glfwTerminate();
+        fprintf(stderr, "GLFW failed to create the window.\n");
+        fprintf(stderr, "Exiting.\n");
+        exit(1);
+	}
+	glfwMakeContextCurrent( window );
+
+	glewExperimental = GL_TRUE;
+	if( glewInit() != GLEW_OK ) {
+        fprintf(stderr, "GLEW failed to init.\n");
+        fprintf(stderr, "Exiting.\n");
+        exit(1);
+	}
+
+	glViewport( 0, 0, width, height );
+
+    printf( "VENDOR = %s\n", glGetString( GL_VENDOR ) ) ;
+    printf( "RENDERER = %s\n", glGetString( GL_RENDERER ) ) ;
+    printf( "VERSION = %s\n", glGetString( GL_VERSION ) ) ;
+}
